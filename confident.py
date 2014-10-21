db2c25c265273e0b19195cce6dfd8c5da21ea6db
Gonzalez-Brenes, Jose P <Jose.Gonzalez-Brenes@pearson.com>
2014-10-20 20:51:21 -0400

    added PyMC3 code

--- confident.py
+++ confident.py
@@ -1,8 +1,20 @@
 __author__ = 'Jose'
 
 import numpy as np
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
 from collections import Counter
 import operator
+import pymc as pm
+import matplotlib.pylab as pl
+'''
+Requires pandas and statsmodels
+
+To install:
+
+pip isntall pandas
+pip install git+https://github.com/pymc-devs/pymc
+'''
 
 class Observation:
     def __init__(self, id, value):
@@ -22,7 +34,7 @@ class Observation:
 
 class TimePoints(list): #list of list of observations
 
-
+    binary_observations = sm.families.Binomial()
 
     def independent_sample_variance_estimator(self):
         '''
@@ -30,6 +42,80 @@ class TimePoints(list): #list of list of observations
         :return: variance estimator
         '''
         return  [np.var( TimePoints.get_values(timepoint) ) for timepoint in self]
+
+    def pooled_sample_variance_estimator(self):
+        #TODO: implement
+        pass
+
+    @staticmethod
+    def invlogit(x):
+         import theano.tensor as t
+         return t.exp(x) / (1 + t.exp(x))
+    def foo(self, discrete):
+        student_ids  = []
+        timestep_ids = []
+        y = []
+
+        #for t in range(0, len(self)):
+        #    student_ids  +=  [o.id for o in self[t]]
+        #    timestep_ids +=  [t    for o in self[t]]
+        #    y            +=  [o.value for o in self[t]]
+
+        student_ids =  [0, 0, 0, 1, 1, 1]
+        timestep_ids = [0, 0, 0, 0, 0, 0]
+        y            = [0, 0, 0, 1, 0, 1]
+
+        n_students  =  len(set(student_ids))
+        n_timesteps = len(self)
+
+        print student_ids, "!", n_students
+
+        with pm.Model() as hierarchical_model:
+            # Hyperpriors for group nodes
+            mu_student = pm.Normal('mu_student', mu=0., sd=100**2)
+            sigma_student = pm.Uniform('sigma_student', lower=0, upper=100)
+
+            #mu_timestep = pm.Normal('mu_beta', mu=0., sd=100**2)
+            #sigma_timestep = pm.Uniform('sigma_beta', lower=0, upper=100)
+
+            student  = pm.Normal('student', mu=mu_student, sd=sigma_student, shape=n_students) #random effect
+            timestep = pm.Normal('timestep', mu=0, sd=100**2, shape=n_timesteps)  #fixed effect
+
+            # Model error
+            eps = pm.Uniform('eps', lower=0, upper=100)
+
+
+            theta = student[student_ids] + timestep[timestep_ids]
+
+            # Data likelihood
+            if discrete:
+                ll = pm.Bernoulli('theta', p=self.invlogit(theta), observed=y)
+            else:
+                ll = pm.Normal('theta', mu=theta, sd=eps, observed=y)
+
+        with hierarchical_model:
+            print "Find MAP..."
+            start = pm.find_MAP()
+            #if discrete:
+            #     step = pm.BinaryMetropolis(scaling=start)
+            # else:
+            print "NUTS..."
+            step = pm.NUTS(scaling=start)
+            print "Samples..."
+            hierarchical_trace = pm.sample(1000, step, start=start, progressbar=False)
+        print "done..."
+        print "Plot..."
+
+        pl.figure(figsize=(10,10))
+        f = pm.traceplot(hierarchical_trace)
+        f.savefig("a.png")
+        return hierarchical_trace
+
+
+
+    @staticmethod
+    def decorrelate(df, family = binary_observations):
+        results = sm.glm('output ~ id + C(timestep) - 1', family=family, data=df).fit()
 
     def correlated_variance_estimator(self, previous=1):
         '''
@@ -82,7 +168,7 @@ def main():
     a = TimePoints([ [  Observation(1, 1), Observation(2, 0), Observation(3, 0), Observation(4, 0)],
                      [  Observation(1, 1), Observation(2, 1), Observation(3, 0), Observation(4, 0)] ])
     print "Independent:", a.independent_sample_variance_estimator()
-    print "Dependent:", a.correlated_variance_estimator()
+    print "Dependent:", a.foo(True)
 
 
 
